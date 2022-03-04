4131ddfa0392c42c61d4f75b1c41e5aa31a9531f
yuhi-ta <yuhi1307@gmail.com>
2022-03-04 19:51:38 +0900

    contoroller修正

--- app/controllers/list_controller.rb
+++ app/controllers/list_controller.rb
@@ -14,6 +14,18 @@ class ListController < ApplicationController
     end
   end
 
+  def edit
+    @list = List.find(id:params[:id])
+  end
+
+  def update
+    @list = List.find_by(id:params[:id])
+    if @list.update_attributes(list_params)
+      redilect_to :root
+    else
+      render acrion: :edit
+    end
+  end
   private
   def list_params
     params.require(:list).permit(:title).merge(user: current_user)
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
 
   devise_for :users
  root 'top#index'
- resources :list, only: %i(new create)
+ resources :list, only: %i(new create edit update)
 
 end
